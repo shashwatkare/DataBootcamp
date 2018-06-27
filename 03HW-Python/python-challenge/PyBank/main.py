3c815486136d7ed9b133f0d00a0bec1d67329d21
Chu-Chun Huang <joy.cchuang@gmail.com>
2018-06-27 14:40:52 -0700

    03HW part 1 done

---
+++ 03HW-Python/python-challenge/PyBank/main.py
@@ -0,0 +1,28 @@
+import csv
+import pandas as pd
+
+input_fili = '~/UCBERKDATA/GitHub/DataBootCampHW/03HW-Python/python-challenge/PyBank/Resources/budget_data.csv'
+output_fili = 'analysis.txt'
+df = pd.read_csv(input_fili)
+
+total_num_month = len(df)
+net_amount_profit = df['Profit/Losses'].sum()
+df['Profit Change'] = df['Profit/Losses'].diff()
+avg_change_profit = df['Profit Change'].mean()
+max_profit_date = df.loc[df['Profit Change'].idxmax(), 'Date']
+max_profit_amount = df.loc[df['Profit Change'].idxmax(), 'Profit Change']
+min_profit_date = df.loc[df['Profit Change'].idxmin(), 'Date']
+min_profit_amount = df.loc[df['Profit Change'].idxmin(), 'Profit Change']
+
+outputstring = 'Financial Analysis\n' + \
+               '=============================\n' + \
+               'Total Months: ' + str(total_num_month) + '\n' + \
+               'Total: {:d}'.format(net_amount_profit) + '\n' + \
+               'Average Change: {:.2f}'.format(avg_change_profit) + '\n' + \
+               'Greatest Increase in Profits: ' + str(max_profit_date) + ' ($ {:.0f}'.format(max_profit_amount) + ')\n' + \
+               'Greatest Decrease in Profits: ' + str(min_profit_date) + ' ($ {:.0f}'.format(min_profit_amount) + ')\n'
+
+print (outputstring)
+
+with open(output_fili,'w') as txt_file:
+  txt_file.write (outputstring)
